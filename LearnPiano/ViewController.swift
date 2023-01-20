c9d77a911e623890c3b538a28eccaea6a1cffa31
AlexeyNikiforov <oktrus@gmail.com>
2023-01-20 23:31:11 +0500

    fix

--- LearnPiano/ViewController.swift
+++ LearnPiano/ViewController.swift
@@ -6,11 +6,15 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
+    
+   // private let conductor = MIDIMonitorConductor()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print(">>>")
         // Do any additional setup after loading the view.
     }
 
